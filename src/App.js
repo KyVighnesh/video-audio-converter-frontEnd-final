bbdfa38885333846c6229a911de9b05255084e73
Vighneswar <vighneshwar.s.10@gmail.comm>
2023-09-25 18:16:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,94 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios'
+import { useState } from 'react';
+
+function App() {
+
+  const [file,setFile]=useState("");
+
+  const[converted,setConverted] = useState("")
+
+  const [loading,setLoading] = useState()
+ 
+
+  const handleFileChange=(event)=>{
+    setFile(event.target.files[0])
+    
+
+}
+
+
+  const videoUpload = () => {
+    let formData=new FormData();
+    formData.append("file",file)
+    console.log(file)
+
+    setLoading(true)
+
+    // we have to send it to our api
+
+    let config = {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        },
+      }
+
+      axios.post("http://localhost:8090/upload",formData,config).then(data=> {
+        console.log(data)
+      
+        setConverted(data.data.fileLink)
+
+        if(data) {
+          setLoading(false)
+        }
+      }).catch(err => {{
+        if(err) {
+          alert("File Not Supported")
+        }
+        console.log(err)  
+      }})
+
+      
+
+  }
+
+  
+
+  return (
+    <div className="App">
+      <h3>Video to Audio Converter Online</h3>
+
+      <input type="file" onChange={handleFileChange}/>
+      <div>
+        <br/>
+        <br/>
+      <button onClick={videoUpload}> Convert </button>
+      </div>
+
+      <br/>
+        <br/>
+      
+      <div>
+        {
+          loading?"processing.....":""
+        }
+
+        {
+          loading == false?<a
+          href={converted}
+          download = 'file.mp3'
+        >
+          <button>Audio is Ready</button>
+        </a>:""
+        }
+
+
+      </div>
+
+      
+    </div>
+  );
+}
+
+export default App;
